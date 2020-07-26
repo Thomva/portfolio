b721389f728da6513b55e638cbd2fa8f4f514f6f
Thomva <thomvadev@gmail.com>
2020-07-26 20:29:34 +0200

    Updates

---
+++ precache-manifest.e334ab18b3a8d503318b8a2cc5488628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6212b5e522c0df23898fe61b4ac4032",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2bf8acd7501f36f71620",
+    "url": "/portfolio/static/css/main.bd3494d2.chunk.css"
+  },
+  {
+    "revision": "52e7961c0efdd529e1a8",
+    "url": "/portfolio/static/js/2.c3793fe1.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/portfolio/static/js/2.c3793fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf8acd7501f36f71620",
+    "url": "/portfolio/static/js/main.db454209.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "b89c4cd3a79598b9ad649c95c8b5e3d0",
+    "url": "/portfolio/static/media/Meso_White.b89c4cd3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c4c665311825980c5c9c8871c60cd4d9.js"
+  "/portfolio/precache-manifest.e334ab18b3a8d503318b8a2cc5488628.js"
 );
 
 self.addEventListener('message', (event) => {
