d8289dd39c7ad9c805956ff2aef41a3ef8b4db9a
Thomva <thomvadev@gmail.com>
2020-10-01 20:37:01 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.cef23f6bfbacf6f885a4a8eb95c33ac4.js"
+  "/portfolio/precache-manifest.5711a0a0189babd8cc02ee70be240fcc.js"
 );
 
 self.addEventListener('message', (event) => {
